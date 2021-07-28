dc7414673ca5408207073e441aaf5decafebc8c7
deepgodara <deep@hierd.co.uk>
2021-07-28 11:46:51 +0100

    Update header.data.js

--- src/components/header/header.data.js
+++ src/components/header/header.data.js
@@ -15,6 +15,13 @@ export default [
     path: 'https://siliconroundabout.tech/',
     label: 'Community',
   },
+<<<<<<< Updated upstream
+=======
+  // {
+  //   path: '/docuseries',
+  //   label: 'How to Start UP',
+  // },
+>>>>>>> Stashed changes
   {
     path: 'https://siliconroundabout.tech/the-silicon-roundabout-blog/',
     label: 'Blog',
