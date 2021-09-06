d531d6ef61a61006a6fcaaa0595101182ca20eca
Francesco Perticarari <23483149+fpert041@users.noreply.github.com>
2021-09-06 16:36:49 +0100

    added startups we raised for via FI 2019

--- src/pages/launchpad.js
+++ src/pages/launchpad.js
@@ -1,36 +1,36 @@
-import React from 'react';
-import { StickyProvider } from 'contexts/app/app.provider';
+import React from "react";
+import { StickyProvider } from "contexts/app/app.provider";
 
-import SEO from 'components/seo';
-import Community_Layout from 'components/community_layout';
+import SEO from "components/seo";
+import Community_Layout from "components/community_layout";
 
-import Founders from 'sections/fi-draft';
-import FoundersAlumni from 'sections/founders-alumni';
-import Criteria from 'sections/criteria';
-import FoundersPartner from 'sections/founders-partner';
-import FoundersTestimonial from 'sections/founders-testimonial';
-import BackToVentures from 'sections/back_to_ventures';
+import Founders from "sections/launchpad/fi-draft";
+import FoundersAlumni from "sections/launchpad/founders-alumni";
+import Criteria from "sections/launchpad/criteria";
+import FoundersPartner from "sections/launchpad/founders-partner";
+import FoundersTestimonial from "sections/launchpad/founders-testimonial";
+import BackToVentures from "sections/launchpad/back_to_ventures";
 
-import 'rc-drawer/assets/index.css';
-import 'react-modal-video/css/modal-video.min.css';
-import 'swiper/swiper-bundle.min.css';
-import 'typeface-dm-sans';
+import "rc-drawer/assets/index.css";
+import "react-modal-video/css/modal-video.min.css";
+import "swiper/swiper-bundle.min.css";
+import "typeface-dm-sans";
 
 export default function IndexPage() {
   return (
     <StickyProvider>
-        <Community_Layout>
-          <SEO
-            description="Silicon Roundabout presents a 4-month startup programme for our community startups in accordance with Founders Institute"
-            title="4-month Startup Programme"
-          />
-          <Founders />
-          <Criteria />
-          <FoundersPartner />
-          <FoundersAlumni />
-          <FoundersTestimonial />
-          <BackToVentures/>
-        </Community_Layout>
+      <Community_Layout>
+        <SEO
+          description="Silicon Roundabout presents a 4-month startup programme for our community startups in accordance with Founders Institute"
+          title="4-month Startup Programme"
+        />
+        <Founders />
+        <Criteria />
+        <FoundersPartner />
+        <FoundersAlumni />
+        <FoundersTestimonial />
+        <BackToVentures />
+      </Community_Layout>
     </StickyProvider>
   );
 }
