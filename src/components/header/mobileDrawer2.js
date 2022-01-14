c04ab43d263f1e6eb2393fc9640007b0734594a6
Francesco Perticarari <23483149+fpert041@users.noreply.github.com>
2022-01-14 11:33:51 +0000

    fix last warnings

--- src/components/header/mobileDrawer2.js
+++ src/components/header/mobileDrawer2.js
@@ -1,5 +1,5 @@
 import React, { useContext } from "react";
-import { Button, Box, Text } from "theme-ui";
+import { Box, Text } from "theme-ui";
 import { Scrollbars } from "react-custom-scrollbars";
 import Drawer from "components/drawer";
 import { DrawerContext } from "contexts/drawer/drawer.context";
